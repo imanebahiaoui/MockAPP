20040e7cddd4804406ae2b04c1129b1ecee433c4
Younes Khoubza <younes.khoubza@gmail.com>
2016-09-09 14:52:08 +0100

    MKAPP-6 : test login is required

---
+++ tests/AppBundle/Entity/UserTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class UserTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var  ValidatorInterface */
+    private $validator;
+
+    /** @var  User */
+    private $user;
+
+    const ATTR_LOGIN = 'login';
+    const ATTR_PASSWORD = 'password';
+
+    const LOGIN_MIN_LENGTH = 3;
+    const PASSWORD_MIN_LENGTH = 6;
+
+
+    protected function setUp()
+    {
+        $this->validator = Validation::createValidatorBuilder()
+                                     ->enableAnnotationMapping()
+                                     ->getValidator();
+        $this->user      = new User();
+    }
+
+
+    public function testLoginIsRequired()
+    {
+        $errors = $this->validator->validateProperty($this->user, self::ATTR_LOGIN);
+        $this->assertTrue(count($errors) > 0);
+
+        $this->user->setLogin('yoeunes');
+        $errors = $this->validator->validateProperty($this->user, self::ATTR_LOGIN);
+        $this->assertEquals(0, count($errors));
+    }
+
+}
